eb24b596b7181dc58b8c5024a5131593a05166e4
runluo <68489000+RainBowLuoCS@users.noreply.github.com>
2025-06-26 21:18:04 +0800

    Update mm_utils.py

--- openomni/mm_utils.py
+++ openomni/mm_utils.py
@@ -8,7 +8,27 @@ import ast
 from transformers import StoppingCriteria
 from openomni.constants import IMAGE_TOKEN_INDEX
 
+def tokenizer_extra_token(prompt, tokenizer, extra_token=DEFAULT_SPEECH_TOKEN, extra_token_index=SPEECH_TOKEN_INDEX, return_tensors=None):
+    prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split(extra_token)]
 
+    def insert_separator(X, sep):
+        return [ele for sublist in zip(X, [sep]*len(X)) for ele in sublist][:-1]
+
+    input_ids = []
+    offset = 0
+    if len(prompt_chunks) > 0 and len(prompt_chunks[0]) > 0 and prompt_chunks[0][0] == tokenizer.bos_token_id:
+        offset = 1
+        input_ids.append(prompt_chunks[0][0])
+
+    for x in insert_separator(prompt_chunks, [image_token_index] * (offset + 1)):
+        input_ids.extend(x[offset:])
+
+    if return_tensors is not None:
+        if return_tensors == 'pt':
+            return torch.tensor(input_ids, dtype=torch.long)
+        raise ValueError(f'Unsupported tensor type: {return_tensors}')
+    return input_ids
+    
 def select_best_resolution(original_size, possible_resolutions):
     """
     Selects the best resolution from a list of possible resolutions based on the original size.
